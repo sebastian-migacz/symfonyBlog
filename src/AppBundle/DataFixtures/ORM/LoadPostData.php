9e918f47c3163c5790654bcbea56fa16a2aa86ee
sebastian-migacz <vkensaj@gmail.com>
2018-02-01 10:03:21 +0100

    database, testdata, adddata, showdata, pagination

---
+++ src/AppBundle/DataFixtures/ORM/LoadPostData.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kensaj
+ * Date: 30.01.2018
+ * Time: 14:02
+ */
+
+namespace DataFixtures;
+
+
+use AppBundle\AppBundle;
+use Doctrine\Bundle\FixturesBundle\ORMFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+//require_once '/path/to/Faker/src/autoload.php';
+
+class LoadPostData implements ORMFixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+
+
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+        $faker = \Faker\Factory::create();
+        for($i=0; $i<=1000; $i++)
+        {
+            $post =new \AppBundle\Entity\Post();
+            $post ->setTitle($faker->sentences(3));
+            $post ->setLead($faker->text(300));
+            $post ->setContent($faker->text(700));
+            $post ->setCreatedAt($faker ->dateTimeThisMonth);
+
+            $manager ->persist($post);
+            }
+            $manager ->flush();
+    }
+}
